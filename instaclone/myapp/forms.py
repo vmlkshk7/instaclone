dcaafae249eea1d7aa11238262896466e8d88f35
vmlkshk7 <vmlkshk7@gmail.com>
2017-07-23 21:10:41 +0530

    django signup name username password

---
+++ instaclone/myapp/forms.py
@@ -0,0 +1,13 @@
+from django import forms
+from models import UserModel, PostModel
+
+
+class SignUpForm(forms.ModelForm):
+    class Meta:
+        model = UserModel
+        fields=['email','username','name','password']
+
+class LoginForm(forms.ModelForm):
+    class Meta:
+        model = UserModel
+        fields = ['username', 'password']
