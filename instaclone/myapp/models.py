ff65f1fe27cb911e8fd09cf1b11e78789fb5fd9d
vmlkshk7 <vmlkshk7@gmail.com>
2017-07-25 19:50:17 +0530

    login

--- instaclone/myapp/models.py
+++ instaclone/myapp/models.py
@@ -2,6 +2,9 @@ from django.db import models
 
 import uuid
 
+# Create your models here.
+
+
 class UserModel(models.Model):
 	email = models.EmailField()
 	name = models.CharField(max_length=120)
@@ -19,3 +22,41 @@ class SessionToken(models.Model):
 
 	def create_token(self):
 		self.session_token = uuid.uuid4()
+
+class PostModel(models.Model):
+	user = models.ForeignKey(UserModel)
+	image = models.FileField(upload_to='user_images')
+	image_url = models.CharField(max_length=255)
+	caption = models.CharField(max_length=240)
+	created_on = models.DateTimeField(auto_now_add=True)
+	updated_on = models.DateTimeField(auto_now=True)
+	has_liked = False
+
+
+	@property
+	def like_count(self):
+		return len(LikeModel.objects.filter(post=self))
+
+	@property
+	def comments(self):
+		return CommentModel.objects.filter(post=self).order_by('-created_on')
+
+class LikeModel(models.Model):
+	user = models.ForeignKey(UserModel)
+	post = models.ForeignKey(PostModel)
+	created_on = models.DateTimeField(auto_now_add=True)
+	updated_on = models.DateTimeField(auto_now=True)
+
+
+class CommentModel(models.Model):
+	user = models.ForeignKey(UserModel)
+	post = models.ForeignKey(PostModel)
+	comment_text = models.CharField(max_length=555)
+	created_on = models.DateTimeField(auto_now_add=True)
+	updated_on = models.DateTimeField(auto_now=True)
+
+class Post_Like(models.Model):
+	user = models.ForeignKey(UserModel)
+	post = models.ForeignKey(PostModel)
+	created_on = models.DateTimeField(auto_now_add=True)
+	updated_on = models.DateTimeField(auto_now=True)
--- instaclone/myapp/views.py
+++ instaclone/myapp/views.py
@@ -1,10 +1,15 @@
 from django.shortcuts import render, redirect
-from forms import SignUpForm, LoginForm, PostForm
-from models import UserModel, SessionToken, PostModel
+from forms import SignUpForm, LoginForm, PostForm, LikeForm, CommentForm
+from models import UserModel, SessionToken, PostModel, LikeModel, CommentModel
 from django.contrib.auth.hashers import make_password, check_password
 from datetime import timedelta
 from django.utils import timezone
+from instaclone.settings import BASE_DIR
 
+from imgurpython import ImgurClient
+
+
+# Create your views here.
 
 def signup_view(request):
     if request.method == "POST":
@@ -52,8 +57,80 @@ def login_view(request):
     return render(request, 'login.html', response_data)
 
 
+def post_view(request):
+    user = check_validation(request)
+
+    if user:
+        if request.method == 'POST':
+            form = PostForm(request.POST, request.FILES)
+            if form.is_valid():
+                image = form.cleaned_data.get('image')
+                caption = form.cleaned_data.get('caption')
+                post = PostModel(user=user, image=image, caption=caption)
+                post.save()
+
+                path = str(BASE_DIR + post.image.url)
+
+                client = ImgurClient('7a6b6f3c625804c', 'e8737b3a742e79b4ef2ee548d556b05893771eb0')
+                post.image_url = client.upload_from_path(path, anon=True)['link']
+                post.save()
+
+                return redirect('/feed/')
+
+        else:
+            form = PostForm()
+        return render(request, 'post.html', {'form': form})
+    else:
+        return redirect('/login/')
+
+
 def feed_view(request):
-    return render(request, 'feed.html')
+    user = check_validation(request)
+    if user:
+
+        posts = PostModel.objects.all().order_by('created_on')
+
+        for post in posts:
+            existing_like = LikeModel.objects.filter(post_id=post.id, user=user).first()
+            if existing_like:
+                post.has_liked = True
+
+        return render(request, 'feed.html', {'posts': posts})
+    else:
+
+        return redirect('/login/')
+
+
+def like_view(request):
+    user = check_validation(request)
+    if user and request.method == 'POST':
+        form = LikeForm(request.POST)
+        if form.is_valid():
+            post_id = form.cleaned_data.get('post').id
+            existing_like = PostLike.objects.filter(post_id=post_id, user=user).first()
+            if not existing_like:
+                LikeModel.objects.create(post_id=post_id, user=user)
+            else:
+                existing_like.delete()
+            return redirect('/feed/')
+    else:
+        return redirect('/login/')
+
+
+def comment_view(request):
+    user = check_validation(request)
+    if user and request.method == 'POST':
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            post_id = form.cleaned_data.get('post').id
+            comment_text = form.cleaned_data.get('comment_text')
+            comment = CommentModel.objects.create(user=user, post_id=post_id, comment_text=comment_text)
+            comment.save()
+            return redirect('/feed/')
+        else:
+            return redirect('/feed/')
+    else:
+        return redirect('/login')
 
 
 # For validating the session
@@ -61,6 +138,8 @@ def check_validation(request):
     if request.COOKIES.get('session_token'):
         session = SessionToken.objects.filter(session_token=request.COOKIES.get('session_token')).first()
         if session:
-            return session.user
+            time_to_live = session.created_on + timedelta(days=1)
+            if time_to_live > timezone.now():
+                return session.user
     else:
         return None
